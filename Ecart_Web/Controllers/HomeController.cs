85bf2576f14c74915472a238ef435adcedb42262
Katleho Mokhethi <Katlehomokhethi1@gmail.com>
2022-10-21 01:18:50 +0200

    View Model for Home Page

--- Ecart_Web/Controllers/HomeController.cs
+++ Ecart_Web/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using Ecart_Web.Models;
+﻿using Ecart_Web.Data;
+using Ecart_Web.Models;
+using Ecart_Web.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace Ecart_Web.Controllers
@@ -7,15 +10,22 @@ namespace Ecart_Web.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
         {
-            return View();
+            HomeVM homeVM = new HomeVM()
+            {
+                Products = _db.Product.Include(u => u.Category).Include(u => u.ApplicationType),
+                Categories = _db.Category
+            };
+            return View(homeVM);
         }
 
         public IActionResult Privacy()

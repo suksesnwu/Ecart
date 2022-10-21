2655d5ad98e892c008c5981d7ccecb067f469505
Katleho Mokhethi <Katlehomokhethi1@gmail.com>
2022-10-21 02:03:26 +0200

    Add to Session - DetailsPost in HomeController

--- Ecart_Web/Controllers/HomeController.cs
+++ Ecart_Web/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Ecart_Web.Data;
 using Ecart_Web.Models;
 using Ecart_Web.Models.ViewModels;
+using Ecart_Web.Utility;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
@@ -38,6 +39,20 @@ namespace Ecart_Web.Controllers
             };
 
             return View(DetailsVM);
+        }
+
+        [HttpPost, ActionName("Details")]
+        public IActionResult DetailsPost(int id)
+        {
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart) != null
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count() > 0)
+            {
+                shoppingCartList = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+            }
+            shoppingCartList.Add(new ShoppingCart { ProductId = id });
+            HttpContext.Session.Set(WC.SessionCart, shoppingCartList);
+            return RedirectToAction(nameof(Index));
         }
 
         public IActionResult Privacy()
